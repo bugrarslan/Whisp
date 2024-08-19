e923533662fe9a8b2b191730aa6371ed81b266ba
bugrarslan <bugrarsln7@gmail.com>
2024-08-19 23:28:27 +0300

    user Profie - 1

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,11 +1,17 @@
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import { useAuth } from "../../contexts/AuthContext";
 import { supabase } from "../../lib/supabase";
+import { wp, hp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Icon from "../../assets/icons";
+import { useRouter } from "expo-router";
+import Avatar from "../../components/Avatar";
 
 const Page = () => {
-  const { setAuth } = useAuth();
+  const {user, setAuth } = useAuth();
+  const router = useRouter();
 
   const signOut = async () => {
     const { error } = await supabase.auth.signOut();
@@ -18,11 +24,71 @@ const Page = () => {
 
   return (
     <ScreenWrapper>
-      <Button title="Sign Out" onPress={signOut} />
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>Whisp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={()=> router.push("./notifications")}>
+              <Icon name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+            </Pressable>
+            <Pressable onPress={()=> router.push("./newPost")}>
+              <Icon name="plus" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+            </Pressable>
+            <Pressable onPress={()=> router.push("./profile")}>
+              <Avatar
+                uri={user?.image}
+                size={hp(4.3)}
+                rounded={theme.radius.sm}
+                style={{borderWidth: 2}}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4)
+  },
+  title: {
+    color: theme.colors.primary,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+    borderWidth: 3
+  },
+  icons: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4)
+  },
+  noPosts: {
+    fontSize: hp(2),
+    color: theme.colors.text,
+    textAlign: "center",
+  }
+});
---
+++ app/(main)/newPost.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const newPost = () => {
+  return (
+    <View>
+      <Text>newPost</Text>
+    </View>
+  )
+}
+
+export default newPost
+
+const styles = StyleSheet.create({})
